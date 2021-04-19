58f895a205a75cf192c40d1a821bed865a734b32
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-04-19 18:15:49 +0800

    Editing Product Data

--- routes/admin.js
+++ routes/admin.js
@@ -18,4 +18,7 @@ router.post('/add-product', adminController.postAddProduct);
 //admin/edit-product/(product id)
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+router.post('/edit-product', adminController.postEditProduct);
+
+
 module.exports = router;
